4eb8e1ab8fd1910c84f54304d19949896287df74
Steffen Leistner <sleistner@gmail.com>
2019-12-13 15:35:40 +0100

    fix(main): define description

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "aws_lambda_layer_version" "main" {
   layer_name          = var.layer_name
   source_code_hash    = data.archive_file.layer.output_base64sha256
   compatible_runtimes = var.compatible_runtimes
+  description         = var.description
 }
