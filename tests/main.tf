728c2836792f22bfda2f3f6671d5958e0b7412d1
Steffen Leistner <sleistner@gmail.com>
2020-11-27 13:11:58 +0100

    feat: update terraform to version 0.13
    BREAKING CHANGE: terraform has been updated to version 0.13

--- tests/main.tf
+++ tests/main.tf
@@ -1,9 +1,3 @@
-terraform {
-  required_version = ">= 0.12"
-
-  backend "local" {}
-}
-
 provider "aws" {
   region = "eu-central-1"
 }
--- versions.tf
+++ versions.tf
@@ -1,3 +1,17 @@
 terraform {
-  required_version = "~> 0.12.0"
+  required_version = ">= 0.13"
+  required_providers {
+    archive = {
+      source = "hashicorp/archive"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 }
